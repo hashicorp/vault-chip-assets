25bdda0caf44b32af94d9df40082a449b6a12edb
Iman Randhawa <iman@hashicorp.com>
2021-04-23 17:48:44 -0500

    Init

---
+++ aws/examples/three_clusters_bastion_vpc_peering/outputs.tf
@@ -0,0 +1,73 @@
+output "Deployment_Tag" {
+  value = random_id.deployment_tag.hex
+}
+
+output "Bastion_DNS" {
+  value = aws_instance.bastion.public_dns
+}
+
+output "SSH_Key" {
+  value = aws_key_pair.key.key_name
+}
+
+output "Primary_Vault_Cluster_LB" {
+  value = module.primary_cluster.vault_load_balancer
+}
+
+output "DR_Vault_Cluster_LB" {
+  value = module.dr_cluster.vault_load_balancer
+}
+
+output "EU_Vault_Cluster_LB" {
+  value = module.eu_cluster.vault_load_balancer
+}
+
+output "Jump_to_Primary" {
+  value = "ssh -4 -fNTMS jump_tunnel -i ${aws_key_pair.key.key_name}.pem -L 8200:${module.primary_cluster.vault_load_balancer}:8200 ubuntu@${aws_instance.bastion.public_dns}"
+}
+output "Jump_to_DR" {
+  value = "ssh -4 -fNTMS jump_tunnel -i ${aws_key_pair.key.key_name}.pem -L 8200:${module.dr_cluster.vault_load_balancer}:8200 ubuntu@${aws_instance.bastion.public_dns}"
+}
+output "Jump_to_EU" {
+  value = "ssh -4 -fNTMS jump_tunnel -i ${aws_key_pair.key.key_name}.pem -L 8200:${module.eu_cluster.vault_load_balancer}:8200 ubuntu@${aws_instance.bastion.public_dns}"
+}
+output "Jump_Status" {
+  value = "ssh -S jump_tunnel -O check ubuntu@${aws_instance.bastion.public_dns}"
+}
+output "Jump_Close" {
+  value = "ssh -S jump_tunnel -O exit ubuntu@${aws_instance.bastion.public_dns}"
+}
+
+output "Jump_Instructions" {
+  value = <<EOF
+Use jump command to forward localhost:8200 to connect to the loadbalancer
+of the cluster you would like to connect to. Then configure VAULT_ADDR to
+use http://localhost:8200. When switching clusters, close out prior jump
+tunnel and initiate a new tunnel.
+EOF
+}
+
+output "vpc1_id" {
+  value = module.primary_cluster.vpc_id
+}
+
+output "vpc1_region" {
+  value = var.region1
+}
+
+output "vpc2_id" {
+  value = module.dr_cluster.vpc_id
+}
+
+output "vpc2_region" {
+  value = var.region2
+}
+
+output "vpc3_id" {
+  value = module.eu_cluster.vpc_id
+}
+
+output "vpc3_region" {
+  value = var.region3
+}
+
---
+++ aws/variables.tf
@@ -0,0 +1,72 @@
+variable "prefix" { default = "" }
+variable "ssh_key_name" { default = "" }
+variable "consul_cluster_size" { default = 5 }
+variable "vault_cluster_size" { default = 3 }
+variable "ami_id" { default = "" }
+variable "ami_filter_owners" {
+  description = "When bash install method, use a filter to lookup an image owner and name. Common combinations are 206029621532 and amzn2-ami-hvm* for Amazon Linux 2 HVM, and 099720109477 and ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-* for Ubuntu 18.04"
+  type        = list(string)
+  default     = ["099720109477"]
+}
+variable "ami_filter_name" {
+  description = "When bash install method, use a filter to lookup an image owner and name. Common combinations are 206029621532 and amzn2-ami-hvm* for Amazon Linux 2 HVM, and 099720109477 and ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-* for Ubuntu 18.04"
+  type        = list(string)
+  default     = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+}
+variable "vpc_id" { default = "" }
+variable "subnet_ids" { default = "" }
+variable "consul_ent_license" { default = "" }
+# This is not implimented yet
+#variable "vault_ent_license" { default = "" }
+variable "consul_version" {
+  default = "1.6.3"
+}
+variable "consul_download_url" { default = "" }
+variable "vault_version" {
+  default = "1.3.2"
+}
+variable "vault_download_url" { default = "" }
+variable "cluster_tag_key" { default = "consul-servers" }
+variable "cluster_tag_value" { default = "auto-join" }
+variable "consul_path" { default = "" }
+variable "vault_path" { default = "" }
+variable "consul_user" { default = "" }
+variable "vault_user" { default = "" }
+variable "ca_path" { default = "" }
+variable "cert_file_path" { default = "" }
+variable "key_file_path" { default = "" }
+variable "server" { default = true }
+variable "client" { default = false }
+variable "config_dir" { default = "" }
+variable "data_dir" { default = "" }
+variable "systemd_stdout" { default = "" }
+variable "systemd_stderr" { default = "" }
+variable "bin_dir" { default = "" }
+variable "datacenter" { default = "" }
+variable "autopilot_cleanup_dead_servers" { default = "" }
+variable "autopilot_last_contact_threshold" { default = "" }
+variable "autopilot_max_trailing_logs" { default = "" }
+variable "autopilot_server_stabilization_time" { default = "" }
+variable "autopilot_redundancy_zone_tag" { default = "az" }
+variable "autopilot_disable_upgrade_migration" { default = "" }
+variable "autopilot_upgrade_version_tag" { default = "" }
+variable "enable_gossip_encryption" { default = true }
+variable "gossip_encryption_key" { default = "" }
+variable "enable_rpc_encryption" { default = true }
+variable "environment" { default = "" }
+variable "recursor" { default = "" }
+variable "tags" {
+  description = "Map of extra tags to attach to items which accept them"
+  type        = map(string)
+  default     = {}
+}
+variable "enable_acls" { default = true }
+variable "force_bucket_destroy" {
+  description = "Boolean to force destruction of s3 buckets"
+  default     = false
+  type        = bool
+}
+variable "enable_consul_http_encryption" { default = false }
+variable "enable_deletion_protection" { default = true }
+variable "subnet_second_octet" { default = "0" }
+variable "create_bastion" { default = true }

6ab7857c2939e139b1e6be51683ceee9fafbe4a8
Iman Randhawa <iman@hashicorp.com>
2021-12-13 23:24:02 -0600

    Remove EU outputs to avoid confusion.

--- awsflasktiny/outputs.tf
+++ awsflasktiny/outputs.tf
@@ -2,17 +2,17 @@ output "eu_instance_availability_zone" {
   value = aws_instance.eu-web.availability_zone
 }
 
-output "eu_instance_id" {
-  value = aws_instance.eu-web.id
-}
+# output "eu_instance_id" {
+#   value = aws_instance.eu-web.id
+# }
 
-output "eu_instance_public_ip" {
-  value = "http://${aws_instance.eu-web.public_ip}:8000"
-}
+# output "eu_instance_public_ip" {
+#   value = "http://${aws_instance.eu-web.public_ip}:8000"
+# }
 
-output "eu_instance_vpc_id" {
-  value = aws_vpc.eu-vpc.id
-}
+# output "eu_instance_vpc_id" {
+#   value = aws_vpc.eu-vpc.id
+# }
 
 output "project_tag" {
   value = var.project_tag
